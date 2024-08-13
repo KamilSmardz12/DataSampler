79dd8e89e27b2ce56637b9ebc65a52ebcfd11469
Kamil Smardz <kamil.smardz12@gmail.com>
2024-08-13 18:16:14 +0200

    cleaning up

--- src/main/java/sk/utils/InstantUtils.java
+++ src/main/java/sk/utils/InstantUtils.java
@@ -6,12 +6,6 @@ public class InstantUtils {
     private static final int SECONDS_IN_ONE_MIN = 60;
     private static final int FIVE_MIN = 5;
 
-    public static Instant roundToNearestFiveMinutesBack(Instant timestamp) {
-        long epochMinutes = timestamp.getEpochSecond() / SECONDS_IN_ONE_MIN;
-        long roundedMinutesBack = (epochMinutes / FIVE_MIN) * FIVE_MIN;
-        return Instant.ofEpochSecond(roundedMinutesBack * SECONDS_IN_ONE_MIN);
-    }
-
     public static Instant roundToNearestFiveMinutesForward(Instant timestamp) {
         long epochSeconds = timestamp.getEpochSecond();
         long remainder = epochSeconds % (FIVE_MIN * SECONDS_IN_ONE_MIN);
